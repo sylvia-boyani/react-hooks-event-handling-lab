3bf2729ac480c7dd7da5a9bee64facd95fc8327d
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-07-20 00:04:41 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,29 @@
-// Code EyesOnMe Component Here
+// // Code EyesOnMe Component Here
+// function keypad(){
+//     function handleChange(event){
+// console.log("Entering password")
+    
+// }
+// return (<input 
+// type="text"
+// name="password"
+// onChange={handleChange}
+// placeHolder="Entering password"
+// />
+// )
+// }
+
+import React from "react";
+
+function EyesOnMe(){
+    function dealWithFocus(){
+        console.log("Good!");
+    }
+
+function dealWithBlur(){
+    console.log("Hey! Eyes on me!");
+}
+    return(
+        <button onFocus={dealWithFocus} onBlur={dealWithBlur}>Eyes on me</button>
+    );
+}
